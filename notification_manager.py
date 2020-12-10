02c3fa353b9f3280b932a67d67b27fd4c8dd643c
Dhadhazi <dhadhazi@gmail.com>
2020-12-10 09:59:51 +0100

    Project done

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,21 @@
+from twilio.rest import Client
+from dotenv import DotEnv
+dotenv = DotEnv()
+
+TWILIO_SID = dotenv.get('TWILIO_SID')
+TWILIO_AUTH_TOKEN = dotenv.get('TWILIO_TOKEN')
+TWILIO_VIRTUAL_NUMBER = dotenv.get('TWILIO_SEND_NUM')
+TWILIO_VERIFIED_NUMBER = dotenv.get('TWILIO_REC_NUM')
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        print(message.sid)
